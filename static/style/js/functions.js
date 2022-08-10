69730f3133415d9c822635125fc6b154e13ee1d3
LosSantosWhite <69223894+LosSantosWhite@users.noreply.github.com>
2022-08-10 18:32:17 +0300

    added js

---
+++ static/style/js/functions.js
@@ -0,0 +1,11 @@
+function showMore() {
+  const elementBlock = document.getElementById('hideSpec');
+  if (elementBlock.style.opacity === "0") {
+    elementBlock.style.opacity = "1";
+    elementBlock.style.height = "auto";
+  } else {
+    elementBlock.style.opacity = "0";
+    elementBlock.style.height = "0";
+
+  }
+}
